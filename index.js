279cb85bbfebddb0db69dd280ef4efadde6b2559
Jason Bamford <scarfandgoggles@yahoo.co.uk>
2019-02-23 12:45:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function removeFromCart(item) {
   var n = 0;
   console.error("Cart length is " + cart.length);
   for (var i=0; i<cart.length; ++i) {
-    if (cart[i] === item) {
+    if (cart[i].itemName === item) {
       ++n;
       cart = cart.splice(i, 1);
       --i;
